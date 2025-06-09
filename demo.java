778da497e52e3a72c411a22159e462171bdd19af
sadhanachaugule <chougulesadhana72@gmail.com>
2025-06-09 11:02:49 +0530

    Add files via upload

---
+++ demo.java
@@ -0,0 +1,10 @@
+public class Demo2 {
+    public static void main (String[] args) {
+        int i=0;
+        while (i<10) {
+            system.err.println(i); 
+            i =i +0;
+        }
+    }
+}
+
